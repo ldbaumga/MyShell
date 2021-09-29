fba72a86746d196412247bc8947b32dcc5fe3a93
Lucas Daniel Baumgartner <ldbaumga@data.cs.purdue.edu>
2021-09-29 13:17:27 -0400

    \Total:                   0.0 of 70.0\

--- command.cc
+++ command.cc
@@ -109,7 +109,7 @@ void Command::execute() {
     // Setup i/o redirection
     // and call exec
     for (auto & simpleCommand: _simpleCommands) {
-        fprintf(stdout, "%s", simpleCommand->_arguments);
+        std::cout << simpleCommand->_arguments;
     }
     // Clear to prepare for next command
     clear();
