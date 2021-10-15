98150f476c43ebf54ccdbda243accfeab446ca74
Lucas Daniel Baumgartner <ldbaumga@data.cs.purdue.edu>
2021-10-14 22:31:17 -0400

    \Total:                   27.0 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -881,9 +881,8 @@ YY_RULE_SETUP
 
     execvp(arg[1], arg);
     perror("execvp(subshell)");
-    exit(1);
+    _exit(1);
     } else { //END CHILD
-    waitpid(pid, NULL, 0);
     write(pin[1], str.c_str(), str.size());
     dup2(defaultin, 0);
     dup2(defaultout, 1);
@@ -903,7 +902,7 @@ fprintf(stderr, "I made it\n");
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 102 "shell.l"
+#line 101 "shell.l"
 {
   std::string str = std::string(yytext);
   str = str.substr(1, str.size() - 2);
@@ -914,63 +913,63 @@ YY_RULE_SETUP
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 110 "shell.l"
+#line 109 "shell.l"
 {
     return GREATGREATAMP;
 }
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 114 "shell.l"
+#line 113 "shell.l"
 {
     return GREATGREAT;
 }
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
-#line 118 "shell.l"
+#line 117 "shell.l"
 {
     return GREATAMP;
 }
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 122 "shell.l"
+#line 121 "shell.l"
 {
     return TWOGREAT;
 }
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
-#line 127 "shell.l"
+#line 126 "shell.l"
 {
   return GREAT;
 }
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 131 "shell.l"
+#line 130 "shell.l"
 {
     return PIPE;
 }
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 135 "shell.l"
+#line 134 "shell.l"
 {
     return LESS;
 }
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 139 "shell.l"
+#line 138 "shell.l"
 {
     return AMPERSAND;
 }
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 142 "shell.l"
+#line 141 "shell.l"
 {
   /* Assume that file names have only alpha chars */
   std::string str = std::string(yytext);
@@ -981,10 +980,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 149 "shell.l"
+#line 148 "shell.l"
 ECHO;
 	YY_BREAK
-#line 988 "lex.yy.cc"
+#line 987 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -2001,4 +2000,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 149 "shell.l"
+#line 148 "shell.l"
