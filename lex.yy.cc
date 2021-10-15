d39ca397e9ba654acc9c3654f3549bba4435cc86
Lucas Daniel Baumgartner <ldbaumga@data.cs.purdue.edu>
2021-10-14 21:12:32 -0400

    \Total:                   27.0 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -883,6 +883,7 @@ YY_RULE_SETUP
     perror("execvp(subshell)");
     exit(1);
     } else { //END CHILD
+    sleep(10);
     write(pin[1], str.c_str(), str.size());
     close(pin[1]);
 
@@ -907,7 +908,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 106 "shell.l"
+#line 107 "shell.l"
 {
   std::string str = std::string(yytext);
   str = str.substr(1, str.size() - 2);
@@ -918,63 +919,63 @@ YY_RULE_SETUP
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 114 "shell.l"
+#line 115 "shell.l"
 {
     return GREATGREATAMP;
 }
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 118 "shell.l"
+#line 119 "shell.l"
 {
     return GREATGREAT;
 }
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
-#line 122 "shell.l"
+#line 123 "shell.l"
 {
     return GREATAMP;
 }
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 126 "shell.l"
+#line 127 "shell.l"
 {
     return TWOGREAT;
 }
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
-#line 131 "shell.l"
+#line 132 "shell.l"
 {
   return GREAT;
 }
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 135 "shell.l"
+#line 136 "shell.l"
 {
     return PIPE;
 }
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 139 "shell.l"
+#line 140 "shell.l"
 {
     return LESS;
 }
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 143 "shell.l"
+#line 144 "shell.l"
 {
     return AMPERSAND;
 }
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 146 "shell.l"
+#line 147 "shell.l"
 {
   /* Assume that file names have only alpha chars */
   std::string str = std::string(yytext);
@@ -985,10 +986,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 153 "shell.l"
+#line 154 "shell.l"
 ECHO;
 	YY_BREAK
-#line 992 "lex.yy.cc"
+#line 993 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -2005,4 +2006,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 153 "shell.l"
+#line 154 "shell.l"
