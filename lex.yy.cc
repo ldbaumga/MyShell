a8c4efa9f6c4cc798d6a5e7802a05543e79c725b
Lucas Daniel Baumgartner <ldbaumga@data.cs.purdue.edu>
2021-10-15 09:33:35 -0400

    \Total:                   27.0 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -898,10 +898,9 @@ YY_RULE_SETUP
     close(defaultout);
 
 
-    std::string str;
     char c;
     while(read(pout[0], &c, 1)) {
-        if (c == "\n") {
+        if (*c == "\n") {
             c =" ";
         }
         fprintf(stderr, "%c", c);
@@ -914,7 +913,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 113 "shell.l"
+#line 112 "shell.l"
 {
   std::string str = std::string(yytext);
   str = str.substr(1, str.size() - 2);
@@ -925,63 +924,63 @@ YY_RULE_SETUP
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 121 "shell.l"
+#line 120 "shell.l"
 {
     return GREATGREATAMP;
 }
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 125 "shell.l"
+#line 124 "shell.l"
 {
     return GREATGREAT;
 }
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
-#line 129 "shell.l"
+#line 128 "shell.l"
 {
     return GREATAMP;
 }
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 133 "shell.l"
+#line 132 "shell.l"
 {
     return TWOGREAT;
 }
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
-#line 138 "shell.l"
+#line 137 "shell.l"
 {
   return GREAT;
 }
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 142 "shell.l"
+#line 141 "shell.l"
 {
     return PIPE;
 }
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 146 "shell.l"
+#line 145 "shell.l"
 {
     return LESS;
 }
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 150 "shell.l"
+#line 149 "shell.l"
 {
     return AMPERSAND;
 }
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 153 "shell.l"
+#line 152 "shell.l"
 {
   /* Assume that file names have only alpha chars */
   std::string str = std::string(yytext);
@@ -992,10 +991,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 160 "shell.l"
+#line 159 "shell.l"
 ECHO;
 	YY_BREAK
-#line 999 "lex.yy.cc"
+#line 998 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -2012,4 +2011,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 160 "shell.l"
+#line 159 "shell.l"
