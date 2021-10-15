efcad57b60355b56b407f7beb4c4c0cdb96433c2
Lucas Daniel Baumgartner <ldbaumga@data.cs.purdue.edu>
2021-10-15 13:53:51 -0400

    \Part 2 Total:            6.0 of 22.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -1019,7 +1019,7 @@ YY_RULE_SETUP
   for (int i = 0; i < str.size(); i++) {
     if (str.at(i) == '\\') {
         str.erase(str.begin() + i);
-        i += 1;
+        i += 2;
     }
   }
   yylval.cpp_string = new std::string(str);
