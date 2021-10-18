67339c922c1a2a3b70ac212b7d02f449d722a52f
Lucas Daniel Baumgartner <ldbaumga@data.cs.purdue.edu>
2021-10-18 15:48:33 -0400

    \Part 3 Total:            3.0 of 28.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -1061,32 +1061,32 @@ YY_RULE_SETUP
     std::string dollsign = std::to_string(getpid());
 
     while(str.find("${$}") != std::string::npos) {
-        str.replace(str.find(dollarsign), dollarsign.length(), dollsign));
+        str.replace(str.find(dollarsign), dollarsign.length(), dollsign);
     }
 
     std::string question ("${?}");
-    std::string quest = new std::string(getenv("?"));
+    std::string quest = std::string(getenv("?"));
 
     while(str.find("${?}") != std::string::npos) {
         str.replace(str.find(question), question.length(), quest);
     }
 
     std::string exclamation ("${!}");
-    std::string excl = new std::string(gentenv("!"));
+    std::string excl = std::string(gentenv("!"));
 
     while(str.find("${!}") != std::string::npos) {
         str.replace(str.find(exclamation), exclamation.length(), excl);
     }
 
     std::string underscore ("${_}");
-    std::string under = new std::string(getenv("_"));
+    std::string under = std::string(getenv("_"));
 
     while(str.find("${_}") != std::string::npos) {
         str.replace(str.find(underscore), underscore.length(), under);
     }
 
     std::string shell ("${SHELL}");
-    std::string sh = new std::string(getenv("SHELL"));
+    std::string sh = std::string(getenv("SHELL"));
 
     while(str.find("${SHELL}") != std::string::npos) {
         str.replace(str.find(shell), shell.length(), sh);
