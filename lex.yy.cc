acb8e0f0c57497c1600dc04942dc56db58d6efe7
Lucas Daniel Baumgartner <ldbaumga@escher03.cs.purdue.edu>
2021-10-19 10:24:19 -0400

    \Total:                   38.5 of 70.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -1110,7 +1110,7 @@ YY_RULE_SETUP
     int endfound = str.find(end);
     while (found != std::string::npos && endfound != std::string::npos) {
         std::string replace = str.substr(found, endfound);
-        str.replace(found + 2, replace.length(), replace);
+        str.replace(found, replace.length()+3, replace);
         found = str.find(start);
         endfound = str.find(end);
     }
