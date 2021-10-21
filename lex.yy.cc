d235623a747213d03377e403110a85391b9f6d25
Lucas Daniel Baumgartner <ldbaumga@data.cs.purdue.edu>
2021-10-21 13:42:42 -0400

    \Part 3 Total:            6.0 of 28.0\

--- lex.yy.cc
+++ lex.yy.cc
@@ -1781,6 +1781,7 @@ YY_RULE_SETUP
 {
     std::string str = std::string(yytext);
 
+    //move these to run on startup
     setenv("$", std::to_string(getpid()).c_str(), 0);
 
     std::string start("${");
@@ -1812,7 +1813,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 19:
 YY_RULE_SETUP
-#line 161 "shell.l"
+#line 162 "shell.l"
 {
     std::string str = std::string(yytext);
     if (str.at(0) == '$') {
@@ -1884,7 +1885,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 20:
 YY_RULE_SETUP
-#line 230 "shell.l"
+#line 231 "shell.l"
 {
   std::string str = std::string(yytext);
   str = str.substr(1, str.size() - 2);
@@ -1894,7 +1895,7 @@ YY_RULE_SETUP
 	YY_BREAK
 case 21:
 YY_RULE_SETUP
-#line 237 "shell.l"
+#line 238 "shell.l"
 {
   /* Assume that file names have only alpha chars */
   std::string str = std::string(yytext);
@@ -1904,10 +1905,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 22:
 YY_RULE_SETUP
-#line 243 "shell.l"
+#line 244 "shell.l"
 ECHO;
 	YY_BREAK
-#line 1911 "lex.yy.cc"
+#line 1912 "lex.yy.cc"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -2924,4 +2925,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 243 "shell.l"
+#line 244 "shell.l"
