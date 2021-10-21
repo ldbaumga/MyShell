82f239febdae1742a862f64ba95c133bac9e1390
Lucas Daniel Baumgartner <ldbaumga@data.cs.purdue.edu>
2021-10-21 14:43:37 -0400

    \Part 3 Total:            7.0 of 28.0\

--- command.cc
+++ command.cc
@@ -333,7 +333,7 @@ void Command::execute() {
 
     //set the envirement variable '_' to the last arg of the last command
     int argSize = _simpleCommands.at(size)->_arguments.size();
-    setenv("_", _simpleCommands.at(size)->_arguments.at(argSize)->c_str(), 1);
+    setenv("_", _simpleCommands.at(size-1)->_arguments.at(argSize-1)->c_str(), 1);
 
     // Clear to prepare for next command
     clear();
