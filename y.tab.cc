5d46bcc83479b391d07902ec3cfcaf4307ad2638
Lucas Daniel Baumgartner <ldbaumga@data.cs.purdue.edu>
2021-10-21 19:16:29 -0400

    \Total:                   48.0 of 70.0\

--- y.tab.cc
+++ y.tab.cc
@@ -1881,7 +1881,7 @@ void expandWildcardsIfNecessary(std::string * arg) {
 
     for (int r = 0; r < arg->length(); r++) {
         if (arg->at(r) == dot) {
-            arg->replace(r-1, 1, "\\.");
+            arg->replace(r, 1, "\\.");
             r ++;
             continue;
         }
@@ -1893,7 +1893,7 @@ void expandWildcardsIfNecessary(std::string * arg) {
         }
 
         if (arg->at(r) == star) {
-            arg->replace(r-1, 1, ".*");
+            arg->replace(r, 1, ".*");
             r ++;
             continue;
         }
