b6ae361b05e0e54664098445c9f3baa860b445f3
Lucas Daniel Baumgartner <ldbaumga@data.cs.purdue.edu>
2021-10-22 01:49:43 -0400

    \Part 3 Total:            7.0 of 28.0\

--- y.tab.cc
+++ y.tab.cc
@@ -1927,7 +1927,6 @@ void expandWildcardsIfNecessary(std::string * arg) {
     }
     arg->insert(arg->length(), "$");
     std::vector<std::string> strs;
-    fprintf(stderr, "%s\n", arg->c_str());
     regex_t re;
     int expbuf = regcomp(&re, arg->c_str(), REG_EXTENDED|REG_NOSUB);
     if (expbuf != 0) {
