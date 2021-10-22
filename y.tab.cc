fafe48711f98374c6b91094f9c3b852cda2e488c
Lucas Daniel Baumgartner <ldbaumga@data.cs.purdue.edu>
2021-10-22 01:46:18 -0400

    \Part 3 Total:            11.0 of 28.0\

--- y.tab.cc
+++ y.tab.cc
@@ -1950,7 +1950,7 @@ void expandWildcardsIfNecessary(std::string * arg) {
 
     if (strs.size() == 0) {
         fprintf(stderr, "No Match.\n");
-        Shell::_currentCommand->clear();
+        Shell::_currentCommand.clear();
         return;
     } else {
 
