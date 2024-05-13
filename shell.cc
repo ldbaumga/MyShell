928cb254825ba4d7c7e9a0ffde32b73d7186cf36
ldbaumga <ldbaumgartner01@gmail.com>
2024-05-13 00:15:37 -0500

    Uploading from Purdue's data server

--- shell.cc
+++ shell.cc
@@ -1,31 +1,31 @@
-#include <cstdio>
-#include <unistd.h>
 #include "shell.hh"
+#include <cstdio>
 #include <signal.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 extern "C" void disp(int sig) {
-    fprintf(stderr, "\nsig:%d        Ouch!\n", sig);
-    Shell::prompt();
+  fprintf(stderr, "\nsig:%d        Ouch!\n", sig);
+  Shell::prompt();
 }
 
 extern "C" void zomb_disp(int sig) {
-    pid_t pid = wait3(0, 0, NULL);
+  pid_t pid = wait3(0, 0, NULL);
 
-    while(waitpid(-1, NULL, WNOHANG) > 0) {
-        fprintf(stderr, "\n[%d] exited.\n", pid);
-    }
+  while (waitpid(-1, NULL, WNOHANG) > 0) {
+    fprintf(stderr, "\n[%d] exited.\n", pid);
+  }
 }
 
 int yyparse(void);
 
 void Shell::prompt() {
-  if(isatty(0)) {
-    //This prevents the prompt from being printed to a file
+  if (isatty(0)) {
+    // This prevents the prompt from being printed to a file
     if (getenv("PROMPT") != NULL) {
-        printf(getenv("PROMPT"));
+      printf(getenv("PROMPT"));
     } else {
-        printf("myshell>");
+      printf("myshell>");
     }
     fflush(stdout);
   }
@@ -47,27 +47,27 @@ void shellrc(void) {
 }
 */
 int main(int argc, char **argv) {
-  //Ctrl-C signal handler
+  // Ctrl-C signal handler
   struct sigaction sa;
   sa.sa_handler = disp;
   sigemptyset(&sa.sa_mask);
   sa.sa_flags = SA_RESTART;
-  if(sigaction(SIGINT, &sa, NULL)) {
+  if (sigaction(SIGINT, &sa, NULL)) {
     perror("sigaction");
     exit(2);
   }
 
-  //Zombie signal handler
+  // Zombie signal handler
   struct sigaction zomb;
   zomb.sa_handler = zomb_disp;
   sigemptyset(&zomb.sa_mask);
   zomb.sa_flags = SA_RESTART;
-  if(sigaction(SIGCHLD, &zomb, NULL)) {
+  if (sigaction(SIGCHLD, &zomb, NULL)) {
     perror("sigaction");
     exit(2);
   }
 
-//  shellrc();
+  //  shellrc();
 
   char absPath[256];
   realpath(argv[0], absPath);
